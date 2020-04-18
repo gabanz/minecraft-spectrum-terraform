aa98a067642679200c382490294d823234bf0334
Faiz Azhar <faiz@cloudflare.com>
2020-04-19 03:01:36 +0800

    add Spectrum SSH app

--- spectrum.tf
+++ spectrum.tf
@@ -1,9 +1,10 @@
-# Defines the origin server in the specified format
+# Define the origin server in the specified format
 locals {
-  cloudflare_spectrum_origin = join("", ["tcp://", google_compute_address.minecraft.address, ":25565"])
+  cloudflare_minecraft_origin = join("", ["tcp://", google_compute_address.minecraft.address, ":25565"])
+  cloudflare_ssh_origin = join("", ["tcp://", google_compute_address.minecraft.address, ":22"])
 }
 
-# Define a spectrum application that proxies Minecraft traffic
+# Define a Spectrum application that proxies Minecraft traffic
 resource "cloudflare_spectrum_application" "minecraft_proxy" {
   zone_id      = var.cloudflare_zone_id
   protocol     = "Minecraft"
@@ -14,6 +15,21 @@ resource "cloudflare_spectrum_application" "minecraft_proxy" {
   }
 
   origin_direct = [
-    local.cloudflare_spectrum_origin
+    local.cloudflare_minecraft_origin
+  ]
+}
+
+# Define a Spectrum application that proxies SSH traffic
+resource "cloudflare_spectrum_application" "ssh_proxy" {
+  zone_id      = var.cloudflare_zone_id
+  protocol     = "SSH"
+  traffic_type = "direct"
+  dns {
+    type = "CNAME"
+    name = var.cloudflare_spectrum_hostname
+  }
+
+  origin_direct = [
+    local.cloudflare_ssh_origin
   ]
 }
